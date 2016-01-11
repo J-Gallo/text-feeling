6d3ffa0119b877236f86b06104bd16907a57ae57
Juan Gallo <juani.reicko@gmail.com>
2016-01-11 01:24:33 -0300

    Endpoint: sendText. Still missing DB data

--- routes/text.js
+++ routes/text.js
@@ -5,12 +5,50 @@
 module.exports = function(app) {
 
     var Text = require('../models/text.js'),
+        _ = require('lodash'),
         querystring = require('querystring'),
-        http = require('http');
+        http = require('http'),
+        feeling = {
+            good: ['lindo', 'hermoso', 'bueno', 'gusta', 'copado'],
+            bad: ['feo', 'horrible', 'malo']
+        },
+        keyword = [];
 
     getText = function (req, res) {
-        console.log(req.body);
-        res.send(req.body);
+        var split = req.body.text.split(" ");
+        _.forEach(split, function (n) {
+            _.forEach(feeling.good, function(good) {
+                if (n == good) {
+                    if (keyword.indexOf(n) == -1) {
+                        keyword.push(n);
+                    }
+
+                    //TODO: Buscar en la DB el value
+                    return;
+                }
+            });
+
+            _.forEach(feeling.bad, function(bad) {
+                if (n == bad) {
+
+                    if (keyword.indexOf(n) == -1) {
+                        keyword.push(n);
+                    }
+
+                    //TODO: Buscar en la DB el value
+                    return;
+                }
+            })
+        });
+
+        var obj = {
+            text: req.body.text,
+            keywords: keyword,
+            feel: "none",
+            finalValue: 0
+        };
+
+        res.send(obj);
     };
 
     app.post('/send-text', getText);
